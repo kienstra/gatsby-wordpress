23b34b26dee78488f409ed047ebfc594a1d07a54
Ryan Kienstra <kienstraryan@gmail.com>
2020-10-07 15:10:44 -0500

    Handle child menu items
    Thanks to Zac's course.

--- src/components/mainMenu.js
+++ src/components/mainMenu.js
@@ -2,18 +2,42 @@ import React from "react"
 import { Link, StaticQuery, graphql } from "gatsby"
 
 const MAIN_MENU_QUERY = graphql`
+    fragment MenuFields on WPGraphql_MenuItem {
+        id
+        label
+        url
+    }
+
     query GET_MENU_ITEMS {
         wpgraphql {
             menuItems(where: { location: PRIMARY}) {
                 nodes {
-                    id
-                    label
-                    url
+                    ...MenuFields
+                    childItems {
+                        nodes {
+                            ...MenuFields
+                        }
+                    }
                 }
             }
         }
     }
 `
+
+const renderMenuItem = item => {
+    const hasChild = item.childItems && item.childItems.nodes.length
+
+    return (
+        <li key={item.id}>
+            <Link to={item.url}>{item.label}</Link>
+            {hasChild && renderChildMenu(item)}
+        </li>
+    )
+}
+
+const renderChildMenu = item => {
+    return <ul>{item.childItems.nodes.map(child => renderMenuItem(child))}</ul>
+}
 
 const MainMenu = props => {
     return (
@@ -27,11 +51,7 @@ const MainMenu = props => {
                 return (
                     <nav>
                         <ul>
-                            {menu.map(item => (
-                                <li key={item.id}>
-                                    <Link to={item.url}>{item.label}</Link>
-                                </li>
-                            ))}
+                            {menu.map(item => renderMenuItem(item))}
                         </ul>
                     </nav>
                 )
