62ec2b99f0986c1826f407317df82b12e7c1df12
Ryan Kienstra <kienstraryan@gmail.com>
2020-10-08 22:06:21 -0500

    Thanks to Jason Lengstorf, import block styles from npm package

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,8 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import "@wordpress/block-library/build-style/style.css"
+
 
 import Header from "./header"
 import "../styles/layout.css"
